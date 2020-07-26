8c21bc6b32eef2a12a0925f5e63920e169ff663c
inc_ <67997232+incipious@users.noreply.github.com>
2020-07-26 17:37:09 -0400

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -12,7 +12,6 @@ class Moderation(commands.Cog):
     async def kick(self, ctx, member : discord.Member, *,  reason=None):
         await member.kick(reason=reason)
         embed=discord.Embed(title=f"{member} Kicked", description=f"{member} was kicked for {reason}", color=000000)
-        embed.add_field(name="DIABLO", value="Moderation", inline=False)
         await ctx.send(embed=embed)
     @kick.error
     async def kick_error(self, ctx, error):
@@ -25,7 +24,6 @@ class Moderation(commands.Cog):
     async def ban(self, ctx, member : discord.Member, *,  reason=None):
         await member.ban(reason=reason)
         embed=discord.Embed(title=f"{member} Banned", description=f"{member} was banned for {reason}", color=000000)
-        embed.add_field(name="DIABLO", value="Moderation", inline=False)
         await ctx.send(embed=embed)
     @ban.error
     async def ban_error(self, ctx, error):
@@ -45,7 +43,6 @@ class Moderation(commands.Cog):
             if (user.name, user.discriminator) == (member_name, member_discriminator):
                 await ctx.guild.unban(user)
                 embed=discord.Embed(title=f"{member} was unbanned.", description=f"{member} was unbanned banned from the server.", color=000000)
-                embed.add_field(name="DIABLO", value="Moderation", inline=False)
                 await ctx.send(embed=embed)
                 return
 
