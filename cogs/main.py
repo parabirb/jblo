6ce9b084f48b7f062b65547bc211b41c6fb4e377
inc_ <67997232+incipious@users.noreply.github.com>
2020-08-03 17:45:25 -0400

    Update main.py

--- cogs/main.py
+++ cogs/main.py
@@ -1,5 +1,7 @@
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
+from libdiablo import diabloserv
+import pymongo
 from itertools import cycle
 
 # STILL A WIP, NOTHING IS FUNCTIONAL
@@ -9,11 +11,15 @@ class Main(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-#    offenders = cycle(libdiablo.webcheckban)
-
-    #@tasks.loop(seconds=15)
-    #async def main(self):
-
+   # @commands.Cog.listener()
+    # MAIN BOT FUNCTION, AUTO KICKS OFFENDERS
+  #  async def on_member_join(self, ctx : discord.ctx, member : discord.Member, reason : diabloserv.reason):
+   #     for x in diabloserv.checkban(member.id):
+   #         if diabloserv.checkban(member.id) == member.id:
+  #              await member.ban(reason=diabloserv.retrieveban(member.id).reason)
+   #             await member.ban(reason=reason)
+   #             embed = discord.Embed(title=f"Offender Joined", description=f"{member.name} has attempted to join the server, but was blocked by Diablo.\n**Reason**: {reason}", color=000000)
+   #             await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(Main(client))
