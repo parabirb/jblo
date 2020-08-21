4124e1bdd3c5a33f7de88cfbf482fe4111a54038
inc_ <67997232+incipious@users.noreply.github.com>
2020-08-20 23:53:34 -0400

    Update autoban.py

--- cogs/autoban.py
+++ cogs/autoban.py
@@ -4,18 +4,24 @@ from libdiablo import diabloserv
 
 # STILL A WIP, NOTHING IS FUNCTIONAL
 
-class Main(commands.Cog):
+class Autoban(commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
     @commands.Cog.listener()
     # MAIN BOT FUNCTION, AUTO KICKS OFFENDERS
-    async def on_member_join(self, ctx, member : discord.Member, reason):
-        if diabloserv.checkban(member.id):
-            await member.ban(reason=diabloserv.retrieveban(member.id).reason)
-            embed = discord.Embed(title=f"Offender Joined", description=f"{member.name} has attempted to join the server, but was blocked by Diablo.\n**Reason**: {reason}", color=0xf7f7f7)
+    async def on_member_join(self, ctx, member : discord.Member, reason : diabloserv.retrieveban):
+        if diabloserv.checkban():
+            await member.ban(reason=reason)
+            embed = discord.Embed(title="`OFFENDER JOINED`", description=f"**{member.name}** has attempted to join the server, but was blocked by Diablo.\n**Reason**: {reason}", color=0xf7f7f7)
             await ctx.send(embed=embed)
 
+    @commands.command()
+    # report system, temp
+    async def report(self, ctx):
+        embed = discord.Embed(title="`REPORT`", description="Please use this link for temporary reporting purposes", url='FORM', color=0xf7f7f7)
+        await ctx.author.send(embed=embed)
+
 def setup(client):
-    client.add_cog(Main(client))
+    client.add_cog(Autoban(client))
