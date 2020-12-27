96f42f250fff778755b9c67149d85437cf1d4d60
inc_ <67997232+incipious@users.noreply.github.com>
2020-12-27 15:13:43 -0500

    Update autoban.py

--- cogs/autoban.py
+++ cogs/autoban.py
@@ -22,11 +22,16 @@ class Autoban(commands.Cog):
             overwrites = {
                 ctx.guild.default_role: discord.PermissionOverwrite(send_messages=False)
             }
-            await ctx.guild.create_text_channel(name="diablobans", topic="Lists the offenders that join the server. :warning: MIGHT BE NSFW, DISABLE AT OWN RISK.", overwrites=overwrites, nsfw=True)
+            await ctx.guild.create_text_channel(
+                name="diablobans",
+                topic="Lists the offenders that join the server. :warning: MIGHT BE NSFW, DISABLE AT OWN RISK.",
+                overwrites=overwrites,
+                nsfw=True
+            )
             embed = discord.Embed(
                 description=":thumbsup: Channel successfully created. The server will be notified if an offender joins.",
                 color=0x7289da,
-                timestamp=datetime.datetime.utcnow()
+                timestamp=datetime.utcnow()
             )
             await ctx.send(embed=embed)
         else:
@@ -41,7 +46,7 @@ class Autoban(commands.Cog):
         offense = collection.find_one({"userid":member.id})
         if offense is not None:
             await member.ban(reason=offense["reason"])
-            bans_channel = discord.utils.get(member.guild.text_channels, name='diablobans')
+            bans_channel = discord.utils.get(member.text_channels, name='diablobans')
             if bans_channel is None:
                 overwrites = {
                     member.guild.default_role: discord.PermissionOverwrite(send_messages=False)
@@ -52,6 +57,7 @@ class Autoban(commands.Cog):
                     overwrites=overwrites,
                     nsfw=True
                 )
+
             embed = discord.Embed(
                 description=f'**{member.name}** has attempted to join the server, but was blocked by Diablo.',
                 timestamp=member.joined_at,
@@ -62,48 +68,31 @@ class Autoban(commands.Cog):
             embed.add_field(name='Reason', value=str(offense["reason"]), inline=False)
             await bans_channel.send(embed=embed)
 
-    @commands.Cog.listener()
-    async def on_guild_join(self, guild):
-        self.server_check.start(guild)
-        print("Banloop active.")
-
     # Checks for predators that came into servers before being added to Diablo
     @tasks.loop(minutes=5.0)
-    async def server_check(self, guild):
-        server_members = [member.id for member in guild.members]
-        offense = collection.find({"userid":{"$in":server_members}})
-        if offense is not None:
+    async def server_check(self):
+        for guild in self.client.guilds:
+            server_members = [member.id for member in guild.members]
+            offense = collection.find({"userid": {"$in": server_members}})
+            if offense is not None:
 
-            for person in offense:
-                member_object = discord.utils.get(guild.members, id=person["userid"])
-                await member_object.ban(reason=person["reason"])
+                for person in offense:
+                    member_object = discord.utils.get(guild.members, id=person["userid"])
+                    await member_object.ban(reason=person["reason"])
 
-                bans_channel = discord.utils.get(guild.text_channels, name='diablobans')
-                if bans_channel is None:
-                    overwrites = {
-                        guild.default_role: discord.PermissionOverwrite(send_messages=False)
-                    }
-                    bans_channel = await guild.create_text_channel(
-                        name="diablobans",
-                        topic="Lists the offenders that join the server. :warning: MIGHT BE NSFW, DISABLE AT OWN RISK.",
-                        overwrites=overwrites,
-                        nsfw=True
+                    bans_channel = discord.utils.get(guild.text_channels, name='diablobans')
+
+                    await diabloban_check()
+
+                    embed = discord.Embed(
+                        description=f'**{member_object.name}** was spotted by Diablo and was banned',
+                        color=0x7289da,
+                        timestamp=datetime.utcnow()
                     )
-                embed = discord.Embed(
-                    description=f'**{member_object.name}** was spotted by Diablo and was banned',
-                    color=0x7289da,
-                    timestamp=datetime.datetime.utcnow()
-                )
-                embed.set_thumbnail(url=member_object.avatar_url)
-                embed.set_author(name='OFFENDER JOINED', icon_url=member_object.avatar_url)
-                embed.add_field(name='Reason', value=str(person["reason"]), inline=False)
-                await bans_channel.send(embed=embed)
-
-    @commands.command()
-    # report system, temp
-    async def report(self, ctx):
-        embed = discord.Embed(title="`REPORT`", description="Please use this link for temporary reporting purposes", url='LINK', color=0xf7f7f7)
-        await ctx.author.send(embed=embed)
+                    embed.set_thumbnail(url=member_object.avatar_url)
+                    embed.set_author(name='OFFENDER JOINED', icon_url=member_object.avatar_url)
+                    embed.add_field(name='Reason', value=str(person["reason"]), inline=False)
+                    await bans_channel.send(embed=embed)
 
     # Total number of Diablo offenders
     @commands.command()
@@ -114,7 +103,7 @@ class Autoban(commands.Cog):
             title=":axe: Offenders",
             description=f'There are exactly **{offenders} offenders** on Diablo.',
             color=0x7289da,
-            timestamp=datetime.datetime.utcnow()
+            timestamp=datetime.utcnow()
         )
         await ctx.send(embed=embed)
 
