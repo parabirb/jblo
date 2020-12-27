4653a526f352fd8a5dd888794a7e7ef35ce7d96e
inc_ <67997232+incipious@users.noreply.github.com>
2020-12-27 16:55:22 -0500

    Update basic.py

--- cogs/basic.py
+++ cogs/basic.py
@@ -10,11 +10,19 @@ class Basic(commands.Cog):
 
     #Events
     @commands.Cog.listener()
-    @commands.guild_only()
     # activation procedure
     async def on_ready(self):
-        await self.client.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game("September 19"))
+        await self.client.change_presence(
+            status=discord.Status.idle
+        )
         print('Activated!')
+        await self.server_count.start()
+
+    @tasks.loop(minutes=60.0)
+    async def server_count(self):
+        await self.client.change_presence(
+            activity=discord.Game(f"{len(self.client.guilds)} servers || d.help for commands")
+        )
 
     # Message sent when Diablo joins.
     @commands.Cog.listener()
@@ -23,7 +31,7 @@ class Basic(commands.Cog):
             title="Thanks for adding Diablo!",
             description=f"Thanks for adding Diablo to {guild.name}, here's some information on Diablo:",
             color=0x7289da,
-            timestamp=datetime.datetime.utcnow()
+            timestamp=datetime.utcnow()
         )
         embed.add_field(
             name='About Diablo',
@@ -51,9 +59,12 @@ class Basic(commands.Cog):
 
     # pingy schmingy
     @commands.command()
-    @commands.guild_only()
     async def ping(self, ctx):
-        embed=discord.Embed(title="Pong!", description=f'**Returned at:** {round(self.client.latency * 1000)}ms', color=0x7289da)
+        embed=discord.Embed(
+            title="Pong!",
+            description=f'**Returned at:** {round(self.client.latency * 1000)}ms',
+            color=0x7289da
+        )
         await ctx.send(embed=embed)
 
     @commands.command()
@@ -64,27 +75,37 @@ class Basic(commands.Cog):
         embed.set_thumbnail(
             url=member.avatar_url
         )
-        embed.add_field(name='User ID:', value=member.id, inline=False)
-        embed.add_field(name='Nickname:', value=member.display_name, inline=False)
-        embed.add_field(name='Joined server at:', value=member.joined_at, inline=False)
-        embed.add_field(name='Created at:', value=member.created_at, inline=False)
+        embed.add_field(name='User ID:', value=member.id)
+        embed.add_field(name='Nickname:', value=member.display_name)
+        embed.add_field(name='Joined Server at:', value=member.joined_at.strftime("%A" + ", " + "%B %d %Y" + " at " + "%I" + ":" + "%M %p"))
+        embed.add_field(name='Created at:', value=member.created_at.strftime("%A" + ", " + "%B %d %Y" + " at " + "%I" + ":" + "%M %p"))
+        embed.add_field(name='Server Permissions:', value=member.guild_permissions)
+        embed.add_field(name='Top Role:', value=member.top_role)
+        embed.add_field(name='Is Bot?', value=member.bot)
+        embed.add_field(name='Boosting Since:', value=member.premium_since)
+
         await ctx.send(embed=embed)
     @whois.error
     async def whois_error(self, ctx, error):
-            if isinstance(error, commands.MissingRequiredArgument):
-                embed = discord.Embed(title=f"{ctx.author}'s Information",
-                                      color=0xFBFBFB)
-                embed.set_thumbnail(
-                    url=ctx.author.avatar_url
-                )
-                embed.add_field(name='User ID:', value=ctx.author.id, inline=False)
-                embed.add_field(name='Nickname:', value=ctx.author.display_name, inline=False)
-                embed.add_field(name='Joined server at:', value=ctx.author.joined_at, inline=False)
-                embed.add_field(name='Created at:', value=ctx.author.created_at, inline=False)
-                await ctx.send(embed=embed)
-            if isinstance(error, commands.BadArgument):
-                embed=discord.Embed(description=":octagonal_sign: Be sure check that you wrote out the name correctly.", color=0xCD1F1F)
-                await ctx.send(embed=embed)
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed = discord.Embed(title=f"{ctx.author}'s Information",
+                                  color=0x7289da
+            )
+            embed.set_thumbnail(
+                url=ctx.author.avatar_url
+            )
+            embed.add_field(name='User ID:', value=ctx.author.id)
+            embed.add_field(name='Nickname:', value=ctx.author.display_name)
+            embed.add_field(name='Joined Server at:', value=ctx.author.joined_at.strftime("%A" + ", " + "%B %d %Y" + " at " + "%I" + ":" + "%M %p"))
+            embed.add_field(name='Created at:', value=ctx.author.created_at.strftime("%A" + ", " + "%B %d %Y" + " at " + "%I" + ":" + "%M %p"))
+            embed.add_field(name='Server Permissions:', value=ctx.author.guild_permissions)
+            embed.add_field(name='Top Role:', value=ctx.author.top_role)
+            embed.add_field(name='Is Bot?', value=ctx.author.bot)
+            embed.add_field(name='Boosting Since:', value=ctx.author.premium_since)
+            await ctx.send(embed=embed)
+        if isinstance(error, commands.BadArgument):
+            embed=discord.Embed(description=":octagonal_sign: Be sure check that you wrote out the name correctly.", color=0xCD1F1F)
+            await ctx.send(embed=embed)
 
     # about
     @commands.command()
@@ -92,7 +113,11 @@ class Basic(commands.Cog):
     async def about(self, ctx):
         embed=discord.Embed(
             title="About",
-            description="**DIABLO** is an acronym that stands for **Database Influenced Automated Ban List of Offenders.** Its purpose is to prevent predators such as zoophiles and pedophiles from joining servers with Diablo, preventing any potential harm to people who may be at risk. Diablo also serves as data collection, so we can create one of the most extensive ban lists on Discord to prevent as many predatory incidents as possible. Think of it as this way- what would be more efficient: curing an illness or preventing an illness? We believe prevention is the right course of action, because we can stop many incidents from occuring.",
+            description="**DIABLO** is an acronym that stands for **Database Influenced Automated Ban List of Offenders.** "
+                        "Its purpose is to prevent predators such as zoophiles and pedophiles from joining servers with Diablo, preventing any potential harm to people who may be at risk. "
+                        "Diablo also serves as data collection, so we can create one of the most extensive ban lists on Discord to prevent as many predatory incidents as possible. "
+                        "Think of it as this way- what would be more efficient: curing an illness or preventing an illness? "
+                        "We believe prevention is the right course of action, because we can stop many incidents from occuring.",
             color=0x7289da
         )
         embed.add_field(name="DIABLO", value="A project by incipious", inline=False)
