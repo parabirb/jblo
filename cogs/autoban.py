348a6d2291fd62e98583e78fe7e2c27167204183
ink <67997232+incipious@users.noreply.github.com>
2021-08-17 00:17:57 -0400

    Update autoban.py
    Disabled Diablo autoban task loop due to restructuring.

--- cogs/autoban.py
+++ cogs/autoban.py
@@ -1,17 +1,19 @@
 import discord
 from discord.ext import commands, tasks
 from pymongo import MongoClient
-import datetime
+from datetime import datetime
+import asyncio
 
-cluster = MongoClient('nice try')
-db = cluster["lol"]
-collection = db["lol"]
+cluster = MongoClient('Redacted')
+db = cluster["Redacted"]
+collection = db["Redacted"]
+
+diablocolor = 0x1551b3
 
 class Autoban(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-        self.server_check.start()
 
     # specify a channel where you want diablo log messages
     @commands.command()
@@ -31,7 +33,7 @@ class Autoban(commands.Cog):
             )
             embed = discord.Embed(
                 description=":thumbsup: Channel successfully created. The server will be notified if an offender joins.",
-                color=0x7289da,
+                color=diablocolor,
                 timestamp=datetime.utcnow()
             )
             await ctx.send(embed=embed)
@@ -41,10 +43,11 @@ class Autoban(commands.Cog):
                 color=0xCD1F1F)
             await ctx.send(embed=embed)
 
+    # Automatic ban on database offenders, triggered by user join.
     @commands.Cog.listener()
-     # MAIN BOT FUNCTION, AUTO KICKS OFFENDERS
-    async def on_member_join(self, member):
+    async def on_member_join(self, member : discord.Member):
         offense = collection.find_one({"userid":member.id})
+
         if offense is not None:
             await member.ban(reason=offense["reason"])
             bans_channel = discord.utils.get(member.guild.text_channels, name='diablobans')
@@ -60,58 +63,23 @@ class Autoban(commands.Cog):
                 )
 
             embed = discord.Embed(
-                description=f'**{member.name}** has attempted to join the server, but was blocked by Diablo.',
+                description=f'**{member}** has attempted to join the server, but was blocked by Diablo.',
                 timestamp=member.joined_at,
-                color=0x7289da
+                color=diablocolor
             )
             embed.set_thumbnail(url=member.avatar_url)
             embed.set_author(name='OFFENDER JOINED', icon_url=member.avatar_url)
             embed.add_field(name='Reason', value=str(offense["reason"]), inline=False)
             await bans_channel.send(embed=embed)
-
-    # Checks for predators that came into servers before being added to Diablo
-    @tasks.loop(minutes=5.0)
-    async def server_check(self):
-        for guild in self.client.guilds:
-            server_members = [member.id for member in guild.members]
-            offense = collection.find({"userid": {"$in": server_members}})
-            if offense is not None:
-
-                for person in offense:
-                    member_object = discord.utils.get(guild.members, id=person["userid"])
-                    await member_object.ban(reason=person["reason"])
-
-                    bans_channel = discord.utils.get(member_object.guild.text_channels, name='diablobans')
-                    if bans_channel is None:
-                        overwrites = {
-                            member_object.guild.default_role: discord.PermissionOverwrite(send_messages=False)
-                        }
-                        bans_channel = await member_object.guild.create_text_channel(
-                            name="diablobans",
-                            topic="Lists the offenders that join the server. :warning: MIGHT BE NSFW, DISABLE AT OWN RISK.",
-                            overwrites=overwrites,
-                            nsfw=True
-                        )
-
-                    embed = discord.Embed(
-                        description=f'**{member_object.name}** was spotted by Diablo and was banned',
-                        color=0x7289da,
-                        timestamp=datetime.utcnow()
-                    )
-                    embed.set_thumbnail(url=member_object.avatar_url)
-                    embed.set_author(name='OFFENDER JOINED', icon_url=member_object.avatar_url)
-                    embed.add_field(name='Reason', value=str(person["reason"]), inline=False)
-                    await bans_channel.send(embed=embed)
-
+            
     # Total number of Diablo offenders
     @commands.command()
     @commands.guild_only()
     async def offenders(self, ctx):
-        offenders = collection.count_documents({})
         embed = discord.Embed(
             title=":axe: Offenders",
-            description=f'There are exactly **{offenders} offenders** on Diablo.',
-            color=0x7289da,
+            description=f'There are exactly **{collection.count_documents({})} offenders** on Diablo.',
+            color=diablocolor,
             timestamp=datetime.utcnow()
         )
         await ctx.send(embed=embed)
